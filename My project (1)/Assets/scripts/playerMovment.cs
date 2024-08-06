39c7e985e2696ef69c9b893515cefae154399f84
apisyt <86847964+apisyt@users.noreply.github.com>
2024-08-06 11:37:45 +0200

    skrypt od animacji
    dalej nie działa ale ciul może ktoś inny to naprawi

--- My project (1)/Assets/scripts/playerMovment.cs
+++ My project (1)/Assets/scripts/playerMovment.cs
@@ -7,15 +7,21 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private float _moveSpeed = 5F;
     private Vector2 _movment;
     private Rigidbody2D _rb;
+    private Animator _animator;
 
+    private const string _horizontal = "ho";
+    private const string _vertical = "ve";
     // Start is called before the first frame update
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     private void Update()
     {
         _movment.Set(inputManager.Movment.x, inputManager.Movment.y);
         _rb.velocity = _movment * _moveSpeed;
+        _animator.SetFloat(_horizontal,_movment.x);
+        _animator.SetFloat(_vertical, _movment.y);
     }
 }
