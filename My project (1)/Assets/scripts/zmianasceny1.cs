e3663c26b9deb66cd052f5afb6cbaa1d01ec51fc
apisyt <86847964+apisyt@users.noreply.github.com>
2024-08-08 23:52:18 +0200

    zmiana sceny z cut scenką

--- My project (1)/Assets/scripts/zmianasceny1.cs
+++ My project (1)/Assets/scripts/zmianasceny1.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
-public class zmianasceny1 : MonoBehaviour
+public class CutsceneTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public VideoPlayer videoPlayer;  // Reference to the VideoPlayer component
+    public string nextSceneName;     // The name of the next scene to load after the cutscene
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene(2);
+        if (other.CompareTag("Player"))
+        {
+            // Start playing the cutscene
+            videoPlayer.Play();
+
+            // Subscribe to the VideoPlayer's loopPointReached event to know when the video is finished
+            videoPlayer.loopPointReached += EndReached;
+        }
     }
 
+    void EndReached(VideoPlayer vp)
+    {
+        // Unsubscribe from the event to prevent it from being called multiple times
+        videoPlayer.loopPointReached -= EndReached;
+
+        // Load the next scene
+        SceneManager.LoadScene(nextSceneName);
+    }
 }
